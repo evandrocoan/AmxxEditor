30526941d340c5f4cabb48191339aa5946ea96cf
evandrocoan <evandrocoan@hotmail.com>
2018-07-16 22:12:40 -0300

    Fixed the plugin breaking when on a unsaved view

--- Amxmodx.py
+++ Amxmodx.py
@@ -246,6 +246,10 @@ class AMXXEditor(sublime_plugin.EventListener):
 		view.show_popup(html, 0, location, max_width=700, on_navigate=self.on_navigate)
 
 	def inteltip_function(self, view, region) :
+		file_name   = view.file_name()
+
+		if not file_name:
+			return
 
 		word_region = view.word(region)
 		location 	= word_region.end() + 1
@@ -253,7 +257,7 @@ class AMXXEditor(sublime_plugin.EventListener):
 		doctset 	= set()
 		visited 	= set()
 		found 		= None
-		node 		= nodes[view.file_name()]
+		node 		= nodes[file_name]
 
 		self.generate_doctset_recur(node, doctset, visited)
 
@@ -264,7 +268,7 @@ class AMXXEditor(sublime_plugin.EventListener):
 					break
 
 		if found:
-			print_debug(1, "param2: [%s]" % simple_escape(found[1]))
+			print_debug(4, "param2: [%s]" % simple_escape(found[1]))
 			filename = os.path.basename(found[2])
 
 
