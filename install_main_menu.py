55f424a6825b04b427f059df59cc320d8fb1dcc4
evandrocoan <evandrocoan@hotmail.com>
2018-07-27 17:50:53 -0300

    Updated README.MD license header

--- install_main_menu.py
+++ install_main_menu.py
@@ -7,6 +7,20 @@
 # Install AmxxEditor Main Menus, install and uninstall package menus
 # Copyright (C) 2018 Evandro Coan <https://github.com/evandrocoan>
 #
+#  Redistributions of source code must retain the above
+#  copyright notice, this list of conditions and the
+#  following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above
+#  copyright notice, this list of conditions and the following
+#  disclaimer in the documentation and/or other materials
+#  provided with the distribution.
+#
+#  Neither the name Evandro Coan nor the names of any
+#  contributors may be used to endorse or promote products
+#  derived from this software without specific prior written
+#  permission.
+#
 #  This program is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU General Public License as published by the
 #  Free Software Foundation; either version 3 of the License, or ( at
