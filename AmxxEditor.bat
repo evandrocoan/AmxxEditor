57d5ffd6c709882c8e7ec68fb888b685c2ca4a4e
evandrocoan <evandrocoan@hotmail.com>
2018-08-12 15:14:11 -0300

    Fixed AmxxEditor.bat and AmxxEditor.sh copyright notices

--- AmxxEditor.bat
+++ AmxxEditor.bat
@@ -3,6 +3,21 @@
 @echo off
 
 :: AMXX Plugin Compiler Script
+:: Copyright (C) 2017-2018 Evandro Coan <https://github.com/evandrocoan>
+::
+::  Redistributions of source code must retain the above
+::  copyright notice, this list of conditions and the
+::  following disclaimer.
+::
+::  Redistributions in binary form must reproduce the above
+::  copyright notice, this list of conditions and the following
+::  disclaimer in the documentation and/or other materials
+::  provided with the distribution.
+::
+::  Neither the name Evandro Coan nor the names of any
+::  contributors may be used to endorse or promote products
+::  derived from this software without specific prior written
+::  permission.
 ::
 ::  This program is free software; you can redistribute it and/or modify it
 ::  under the terms of the GNU General Public License as published by the
