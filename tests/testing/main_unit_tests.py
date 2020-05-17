4ef2e076f364d12b8d5c9d9b699608221b766a92
evandrocoan <evandrocoan@hotmail.com>
2020-05-17 15:50:55 -0300

    Created a benchmark Unit Text with galileo.sma

--- tests/testing/main_unit_tests.py
+++ tests/testing/main_unit_tests.py
@@ -38,12 +38,13 @@
 
 
 import os
-import re
 
 import sys
-import pprint
-import textwrap
 import unittest
+
+import io
+import pstats
+import cProfile
 
 import sublime_plugin
 
@@ -225,13 +226,40 @@ class MainUnitTests(unittest.TestCase):
             'PLUGIN_NAME\t doc_string.inc': 'PLUGIN_NAME',
         }, func_list )
 
+    @unittest.skip("Only run it when benchmarking")
+    def test_big_file_parse_time(self):
+        file_name = get_relative_path( 'galileo.sma', __file__ )
+        # log( 1, "file_name: %s", file_name )
+
+        def run_parse():
+            node = Node(file_name)
+            pawnParse = PawnParse()
+
+            with open( node.file_name ) as file:
+                pawnParse.start(file, node)
+
+        profiller = cProfile.Profile()
+        profiller.enable()
+
+        for index in range(10):
+            run_parse()
+
+        profiller.disable()
+        outputstream = io.StringIO()
+
+        profiller_status = pstats.Stats( profiller, stream=outputstream )
+        profiller_status.sort_stats( "time" )
+        profiller_status.print_stats()
+        sys.stderr.write( outputstream.getvalue()[:2000] + '\n' )
+
 
 # https://stackoverflow.com/questions/15971735/running-single-test-from-unittest-testcase-via-command-line/
 def load_tests(loader, standard_tests, pattern):
     suite = unittest.TestSuite()
     suite.addTest( MainUnitTests( 'test_doc_strings' ) )
+    # suite.addTest( MainUnitTests( 'test_big_file_parse_time' ) )
     return suite
 
 # Skip Custom load_tests()
-# load_tests = None
+load_tests = None
 
